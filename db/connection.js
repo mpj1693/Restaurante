15d133a3c09844bb90213f03b3536b5956a279a4
mpj1693 <mpj1693@gmail.com>
2019-05-01 13:58:29 -0400

    deployment

---
+++ db/connection.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+require("dotenv").config();
+
+var connection;
+
+// Sets up db to connect locally or on JAWSDB if deployed
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.db_password,
+    database: "restaurant_db"
+  });
+}
+
+// Turns BOOLEAN 0s and 1s returned from the db into true and false
+connection.config.typeCast = function(field, next) {
+  if (field.type == "TINY" && field.length == 1) {
+    return field.string() == "1"; // 1 = true, 0 = false
+  }
+  return next();
+};
+
+// Export the connection so it's available in other parts of the app
+module.exports = connection;
